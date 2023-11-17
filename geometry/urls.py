4323930651679d00b381a64ddeed44727a41d747
kazacoder <avtk.tlt@gmail.com>
2023-11-17 14:33:46 +0300

    Реализован раздел Геометрия: расчет площадей фигур

--- geometry/urls.py
+++ geometry/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('calculate_geometry/', views.get_rectangle_area),
-    path('', views.get_rectangle_area),
+    path('', views.index),
+    path('rectangle/<width>/<high>', views.get_rectangle_area ),
+    path('square/<side>', views.get_square_area ),
+    path('circle/<radius>', views.get_circle_area ),
 ]
--- geometry/views.py
+++ geometry/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
+from math import pi
 from django.shortcuts import render
 
 
 # Create your views here.
 
-def get_rectangle_area(request):
-    return HttpResponse("test")
+def get_rectangle_area(request, width, high):
+    return HttpResponse(f"Площадь прямоугольника размером {width} х {high} = {int(width) * int(high)}")
+
+
+def get_square_area(request, side):
+    return HttpResponse(f"Площадь квадрата со стороной {side} = {int(side) ** 2}")
+
+
+def get_circle_area (request, radius):
+    return HttpResponse(f"Площадь квадрата с радиусом {radius} = {round(int(radius) ** 2 * pi, 2)}")
+
+
+def index(request):
+    return HttpResponse(f"Геометрия")
