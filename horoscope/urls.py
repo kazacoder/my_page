c0ce7f69e70105962a7152b9079d762b2827d1c2
kazacoder <avtk.tlt@gmail.com>
2023-11-20 10:05:20 +0300

    Переформатирование главного меню приложения Гороскоп - динамическое на основании словаря знаков

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -5,17 +5,5 @@ from . import views
 urlpatterns = [
     path('<int:sign_number>/', views.get_info_about_zodaic_sign_by_number),
     path('<str:sign_zodiac>/', views.get_info_about_zodaic_sign, name='horoscope-name'),
-    # path('aries/', views.aries),
-    # path('taurus/', views.taurus),
-    # path('gemini/', views.gemini),
-    # path('cancer/', views.cancer),
-    # path('leo/', views.leo),
-    # path('virgo/', views.virgo),
-    # path('libra/', views.libra),
-    # path('scorpio/', views.scorpio),
-    # path('sagittarius/', views.sagittarius),
-    # path('capricorn/', views.capricorn),
-    # path('aquarius/', views.aquarius),
-    # path('pisces/', views.pisces),
     path('', views.horoscope),
 ]
