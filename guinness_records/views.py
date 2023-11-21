05b425b28517e6f2d3591a0086c9defc5d7a6e00
kazacoder <avtk.tlt@gmail.com>
2023-11-21 21:06:05 +0300

    рефакторинг приложение Рекорды гиннеса - данные в писке словарей

--- guinness_records/views.py
+++ guinness_records/views.py
@@ -5,10 +5,11 @@ from .records_list import records_list
 
 
 def index(request):
-    data = {'record_list': [record for record in records_list]}
+    data = {'record_list': [(i, record['title']) for i, record in enumerate(records_list)]}
     return render(request, 'guinness_records/record_list.html', data)
 
 
 def record(request, record):
-    data = {'title': record, 'description': records_list[record]['description']}
-    return render(request, 'guinness_records/guinness_record.html', data)
+    data = records_list[int(record)]
+    context = {'title': data['title'], 'description': data['description']}
+    return render(request, 'guinness_records/guinness_record.html', context)
