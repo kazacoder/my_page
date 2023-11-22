7f1ae94c4b53f99a50b973969276f0b048420313
kazacoder <avtk.tlt@gmail.com>
2023-11-22 17:54:20 +0300

    выполнены задания по уроку 3.8 создан базовый шаблон, исправлены шаблоны в приложении Геометрия, Рекорды Гиннесса

--- app_for_tests/people.py
+++ app_for_tests/people.py
@@ -1,23 +1,8 @@
 from faker import Faker
-from datetime import  date
-
-people1 = [
-    'Цветков Алексей Якубович',
-    'Ювеналий Феликсович Лазарев',
-    'Овчинников Измаил Эдуардович',
-    'Анастасия Антоновна Бобылева',
-    'Майя Никифоровна Михайлова',
-    'Радован Владиленович Колобов',
-    'Мухина Виктория Захаровна',
-    'Мокей Захарьевич Виноградов',
-    'Крылова Наталья Романовна',
-    'Милан Викторович Кабанов'
-]
+from datetime import date
 
 faker = Faker('ru_RU')
 
 people = [
     {'name': faker.name(), 'age': date.today().year - int(faker.year()), 'phone': faker.phone_number()} for _ in range(15)
 ]
-
-print(*people)
