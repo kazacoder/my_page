4a0e65c295029d32a57f3b0c44c072654167df69
kazacoder <avtk.tlt@gmail.com>
2023-11-28 17:59:58 +0300

    украшательства + шаблон TemplateView

--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+
 from .forms import FeedbackForm
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Feedback
@@ -32,10 +34,13 @@ class FeedBackView(LoginRequiredMixin, View):
         return render(request, 'feedback/feedback.html', context={'form': form, 'upd': upd})
 
 
-class IndexView(LoginRequiredMixin, View):
-    @staticmethod
-    def get(request):
-        return render(request, 'feedback/feedback_index.html', {'feeds': Feedback.objects.all()})
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'feedback/feedback_index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['feeds'] = Feedback.objects.all()
+        return context
 
 
 class DoneView(LoginRequiredMixin, View):
